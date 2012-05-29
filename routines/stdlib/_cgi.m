3fc9e22882a8ade80bb8ba3dfa671479b705c32f
Sean Woods <sean@seanwoods.com>
2012-05-28 22:49:54 -0400

    Cleaned up CGI routine.

--- routines/stdlib/_cgi.m
+++ routines/stdlib/_cgi.m
@@ -1,37 +1,7 @@
 %cgi ; CGI Handling Routines
- ; @impure %req
- ; Adapted from similar routines by Ed de Moel.
- N i,data,entry
- W "Content-Type: text/html",!,!
- S $ZT="D err^"_$T(+0)_"($ZSTATUS)"
- ;
- I ("REQUEST_METHOD")="POST" D
- . R data#$ZTRNLNM("CONTENT_LENGTH"):5
- . Q
- E  D
- . S data=$ZTRNLNM("QUERY_STRING")
- . Q
- ;
- F i=1:1:$L(data,"&") D
- . N ind,pc,val
- . S pc=$P(data,"&",i)
- . S ind=$$URLin($P(pc,"=",1)),val=$$URLin($P(pc,"=",2))
- . S:ind'="" %req("data",ind)=val
- . Q
- ;
+ ; This file implements all the basic RFC-related functionality for
+ ; interaction with WWW services.
  Q
- ;
-err(msg) ; Output error message
- W "<!doctype html>",!
- W "<html>",!
- W ?2,"<title>GT.M Web Error</title>",!
- W ?2,"<h3>Error: "_$$HTMLout(msg)_"</h3>",!
- W ?2,"<p><em>$ZV="_$ZV_"</em></p>",!
- W ?2,"<pre>",!
- ZSH "*"
- W ?2,"</pre>",!
- W ?2,"</html>",!
- H
  ;
 URLin(url) ; Parse special characters from URL.
  N c,char,hex,i,p,r,z
@@ -69,34 +39,4 @@ HTMLout(html) ; Escape special HTML characters.
  . S r=r_e
  . Q
  Q r
- ;
-envVars(env) ;
- S env("DOCUMENT_ROOT")=1
- S env("GATEWAY_INTERFACE")=1
- S env("HOME")=1
- S env("HTTP_ACCEPT")=1
- S env("HTTP_ACCEPT_CHARSET")=1
- S env("HTTP_ACCEPT_ENCODING")=1
- S env("HTTP_ACCEPT_LANGUAGE")=1
- S env("HTTP_CONNECTION")=1
- S env("HTTP_HOST")=1
- S env("HTTP_USER_AGENT")=1
- S env("PATH")=1
- S env("PATH_INFO")=1
- S env("PATH_TRANSLATED")=1
- S env("QUERY_STRING")=1
- S env("REMOTE_ADDR")=1
- S env("REMOTE_PORT")=1
- S env("REQUEST_METHOD")=1
- S env("REQUEST_URI")=1
- S env("SCRIPT_FILENAME")=1
- S env("SCRIPT_NAME")=1
- S env("SERVER_ADDR")=1
- S env("SERVER_ADMIN")=1
- S env("SERVER_NAME")=1
- S env("SERVER_PORT")=1
- S env("SERVER_PROTOCOL")=1
- S env("SERVER_SIGNATURE")=1
- S env("SERVER_SOFTWARE")=1
- Q
  ;
